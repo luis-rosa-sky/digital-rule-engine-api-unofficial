6a821797a47a2037c277a4ffa49a0c3603ab78e4
luiz-consani-sky <luiz.consani@sky.uk>
2025-01-06 10:19:26 +0000

    Update utils.py
    New LogLevel Enum class

--- src/shared_utils/utils.py
+++ src/shared_utils/utils.py
@@ -3,9 +3,21 @@
 # Standard library imports
 # pylint: disable=unused-import,c-extension-no-member,wrong-import-order
 import logging
+from enum import Enum
+
+class LogLevel(Enum):
+    CRITICAL = 50
+    FATAL = 50
+    ERROR = 40
+    WARNING = 30
+    WARN = 30
+    INFO = 20
+    DEBUG = 10
+    NOTSET = 0
+
 
 # Function to get a logger
-def get_logger(name: str) -> logging.Logger:
+def get_logger(name: str, log_level: LogLevel.value = LogLevel.INFO) -> logging.Logger:
     """
     Get a logger with the specified name and configure it.
 
@@ -17,5 +29,5 @@ def get_logger(name: str) -> logging.Logger:
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
     logger.addHandler(handler)
-    logger.setLevel(logging.INFO)
+    logger.setLevel(log_level)
     return logger
